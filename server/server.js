fed1d3cf715c892639bb501b278a993c9768f121
swarajt123 <swarajtale123@gmail.com>
2026-09-11 22:27:54 +0530

    gemini version updated

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ const genAI = new GoogleGenerativeAI(
 );
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-2.5-flash",
+  model: "gemini-3.1-flash-lite",
 });
 
 /* ----------------------------------
